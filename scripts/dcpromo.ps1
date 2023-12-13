35c7ba7d78c20789af2a2c6e846a33158bf743e4
ABunning <34253384+AndyBunning@users.noreply.github.com>
2023-12-13 11:51:04 +0000

    Update dcpromo.ps1 to apply custom variables to settings

--- scripts/dcpromo.ps1
+++ scripts/dcpromo.ps1
@@ -1,1 +1,19 @@
-dcpromo /unattend:dcpromo_unattend.txt
+<#
+    .SYNOPSIS
+        Promote the VM to a Domain Controller using the dcpromo_unattend.txt settings file.
+#>
+
+param (
+    [String]$dnsDomain,
+    [String]$netbiosDomain,
+    [String]$safeModePass
+)
+
+# Update unattend settings file
+Add-Content -Path "dcpromo_unattend.txt" -Value "NewDomainDNSName=$($dnsDomain)"
+Add-Content -Path "dcpromo_unattend.txt" -Value "DomainNetbiosName=$($netbiosDomain)"
+Add-Content -Path "dcpromo_unattend.txt" -Value "SafeModeAdminPassword=$($safeModePass)"
+
+
+# Run dcpromo with unattend settings file
+Start-Process dcpromo /unattend:dcpromo_unattend.txt
